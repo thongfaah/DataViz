feb1e6cd4a285235679205c9018a68bad01733e1
thongfaah <chayanee.02@mail.kmutt.ac.th>
2025-03-07 07:50:19 +0700

    Update Sidebar

--- src/app/Sidebar/page.jsx
+++ src/app/Sidebar/page.jsx
@@ -137,6 +137,7 @@ import { useRouter } from 'next/navigation'
                             onClick={() => {
                                 handleClick(index);
                                 router.push("/Toolbar");
+                                router.push("/Dashboard");
                             }}
                             
                             className={`flex flex-col items-center justify-center w-full h-20 relative z-10 text-[#2B3A67] text-xs ${
